4b7fd2e28ab6ffb91a60690fe4c57f0551bca614
Moritz Wirger <lj.moritorii@web.de>
2021-12-03 20:57:43 +0100

    Add file type filter to OTA upload

--- code/Constants.h
+++ code/Constants.h
@@ -24,4 +24,4 @@ constexpr static const char* HOSTNAME PROGMEM = "LED Christmas Tree";
 
 /// OTA web page
 constexpr static const char* OTA_INDEX PROGMEM
-    = R"(<!DOCTYPE html><html><head><meta charset=utf-8><title>OTA</title></head><body><div class="upload"><form method="POST" action="/ota" enctype="multipart/form-data"><input type="file" name="data" /><input type="submit" name="upload" value="Upload" title="Upload Files"></form></div></body></html>)";
+    = R"(<!DOCTYPE html><html><head><meta charset=utf-8><title>OTA</title></head><body><div class="upload"><form method="POST" action="/ota" enctype="multipart/form-data"><input type="file" accept=".bin, application/octet-stream" name="data"/><input type="submit" name="upload" value="Upload" title="Upload Files"></form></div></body></html>)";

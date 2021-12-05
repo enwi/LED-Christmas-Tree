830d39cad2a6f3ccfade144b9664f688547af3b8
Moritz Wirger <lj.moritorii@web.de>
2021-12-05 22:07:17 +0100

    Init menu state with default value

--- code/Menu.h
+++ code/Menu.h
@@ -41,7 +41,7 @@ private:
     Callback* actions[3] = {};
     // Selected mode for long press (1 after 1s, 2 after 2s, 3 after 3s, 1 after 1s)
     uint8_t longPressMode = 0;
-    MenuState state;
+    MenuState state = MenuState::mainSelect;
     Callback* brightnessCallback = nullptr;
     uint8_t subSelection = 0;
     uint8_t numSelections = 1;
